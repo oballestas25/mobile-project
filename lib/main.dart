372b0e7489ce8fa05045c02c7952c0a7889c7136
oballestas <oballestas@lsv-tech.com>
2022-06-22 01:09:11 -0500

    Including profiles view

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
-import 'package:mobile_project/view/home_view.dart';
-import 'package:mobile_project/view/list_firebase.dart';
 import 'package:mobile_project/view/splash.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  //callDatabase();
   await Firebase.initializeApp();
   runApp(const Myapp());
 }
-
 
 class Myapp extends StatelessWidget {
   const Myapp({Key? key}) : super(key: key);
@@ -20,12 +15,3 @@ class Myapp extends StatelessWidget {
     return const MaterialApp(debugShowCheckedModeBanner: false, home: Splash());
   }
 }
-
-/* void callDatabase() {
-  DatabaseReference starCountRef =
-      FirebaseDatabase.instance.ref('Registros/');
-  starCountRef.onValue.listen((event) {
-    final data = event.snapshot.value;
-    print(data.toString());
-  });
-} */
---
+++ lib/view/profile.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+class Profile extends StatefulWidget {
+  Map<String,dynamic> profileData;
+  Profile(this.profileData,{Key? key}) : super(key: key);
+
+  @override
+  State<Profile> createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+  static const Color iconColor =  Color.fromARGB(255, 107, 169, 109);
+  static const iconsPadding = EdgeInsets.fromLTRB(30, 0, 0, 0);
+
+  @override
+  Widget build(BuildContext context) {
+   return Scaffold(
+        appBar: AppBar(
+        title: const Text("Developer"),
+        backgroundColor: Colors.cyan,
+      ),
+      
+     body: Column(
+          children: [
+            Padding(
+            padding: const EdgeInsets.all(30),
+            child:
+              GestureDetector(child: CircleAvatar( 
+                backgroundImage: 
+                  AssetImage(widget.profileData["image"]),radius: 130,),
+                onTap: (){
+                  showAlert(context,widget.profileData["image"]);
+                },
+              )
+            ),
+            ListTile(
+              contentPadding: iconsPadding,
+              title:  Text(
+                widget.profileData["name"],
+                style: const TextStyle(fontWeight: FontWeight.w500),
+              ),
+              subtitle: const Text('Developer'),
+              leading: const Icon(
+                Icons.person,
+                color: iconColor,
+              ),
+            ),
+   
+             ListTile(
+              contentPadding: iconsPadding,
+              title: Text(
+                widget.profileData["cel"],
+                style: const TextStyle(fontWeight: FontWeight.w500),
+              ),
+              leading: const Icon(
+                Icons.contact_phone,
+                color: iconColor,
+              ),
+            ),
+   
+            ListTile(
+              contentPadding: iconsPadding,
+              title: Text(widget.profileData["email"]),
+              leading: const Icon(
+                Icons.contact_mail,
+                color:iconColor,
+              ),
+            ),
+          ],
+        ),
+   )
+    
+  ;
+}}
+
+showAlert(BuildContext context,profileImage) {  
+    
+  AlertDialog alert = AlertDialog(  
+    content:  Image( 
+      image: AssetImage(profileImage),
+      
+      )
+      ,
+  );  
+  
+  showDialog(  
+    context: context,  
+    builder: (BuildContext context) {  
+      return alert;  
+    },  
+  );  
+}  
